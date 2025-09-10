2069caf61d3e8b82c168af116cfec2bb4540b9d9
DilinyerCR <dilinyerecr@gmail.com>
2025-09-09 21:42:01 -0400

    new push

---
+++ src/components/Form.tsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react"
+import { v4 as uuidv4} from "uuid"
+import { categories } from "../data/categories"
+import type { Activity } from "../types/types"
+import type { ActivityActions, ActivityState } from "../reducers/activity-reducers"
+
+type FormProps = {
+    dispatch: React.Dispatch<ActivityActions>,
+    state: ActivityState
+}
+
+
+const Form = ({dispatch, state} : FormProps) => {
+
+    const initialState : Activity = {
+        id: uuidv4(),
+        category: 1,
+        name: '',
+        calories: 0
+    }
+
+    const [activity, setActivity] = useState<Activity>(initialState)
+
+    useEffect(() => {
+        if(state.activeId) {
+            const selectedActivity = state.activities.filter( stateActivity => stateActivity.id === state.activeId)[0]
+            setActivity(selectedActivity)
+        }
+    }, [state.activeId])
+    
+    const handleChange = (e : React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => {
+        const isumberField = ['category', 'calories'].includes(e.target.id)
+
+        setActivity({
+            ...activity,
+            [e.target.id]: isumberField ? +e.target.value : e.target.value
+        })
+    }
+
+    const isValidActivity = () => {
+        const { name, calories } = activity
+        return name.trim() !== '' && calories > 0
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        dispatch({ type: 'save-activity', payload: { newActivity: activity } })
+        setActivity(initialState) //En caso de que se repitan los "id", cambiar esta linea de codigo (video 148 min:2:30)
+    }
+
+
+    return (
+        <form className="space-y-5 bg-white shadow p-10 rounded-lg"
+            onSubmit={handleSubmit}
+        >
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="category" className="font-bold">Categoria:</label>
+                <select className="border border-slate-300 p-2 rounded-lg w-full bg-white" 
+                        id="category"
+                        value={activity.category}
+                        onChange={handleChange}
+                    >
+                        {categories.map(category => (
+                            <option 
+                                key={category.id}
+                                value={category.id}
+                            >
+                                {category.name}
+                            </option>
+                        ))}
+                </select>
+            </div>
+
+
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="name" className="font-bold">Actividad:</label>
+                <input 
+                    id="name" 
+                    type="text"
+                    className="border border-slate-300 p-2 rounded-lg"
+                    placeholder="Ej. Comida, jugo de naranja, ensalada, ejercicio, pesas, bicicleta"
+                    value={activity.name}
+                    onChange={handleChange}
+                />          
+            </div>
+
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="calories" className="font-bold">Calorias:</label>
+                <input 
+                    id="calories" 
+                    type="number"
+                    className="border border-slate-300 p-2 rounded-lg"
+                    placeholder="Calorias. ej. 300 o 500"
+                    value={activity.calories}
+                    onChange={handleChange}
+                />       
+            </div>
+
+            <input 
+                type="submit" 
+                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10 disabled:cursor-auto"
+                value={activity.category === 1 ? 'Guardar Comida' : 'Guardar Ejercicio'}
+                disabled={!isValidActivity()}
+            />
+        </form>
+    )
+}
+
+export default Form
